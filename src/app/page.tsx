695dedd6dc202ca78cb8c05053b03c230925fa36
drDaria <ddrobazkina@gmail.com>
2025-01-15 16:19:23 +0200

    Squashed commit of the following:
    commit 494c434f6e2727211c8ad93e06caeecaf659939e
    Author: drDaria <ddrobazkina@gmail.com>
    Date:   Wed Jan 15 16:17:19 2025 +0200
    
        Update page.tsx
    
    commit b55b1c7c223dc24f7607388100768934a1946e60
    Author: drDaria <ddrobazkina@gmail.com>
    Date:   Wed Jan 15 16:14:20 2025 +0200
    
        added styles
    
    commit effac4d5675b96c3cb1d180fbba5c5a959831603
    Author: drDaria <ddrobazkina@gmail.com>
    Date:   Wed Jan 15 15:41:07 2025 +0200
    
        added localStorage to global variables
    
    commit b374b2ebcbfc66be372815687883d4c41cbc4212
    Author: drDaria <ddrobazkina@gmail.com>
    Date:   Wed Jan 15 15:38:19 2025 +0200
    
        style fixes
    
    commit 354ea06a6e635f427a273d69fc7066b54c021852
    Author: drDaria <ddrobazkina@gmail.com>
    Date:   Wed Jan 15 15:38:03 2025 +0200
    
        added localStorage logic
    
    commit 453ba2f95856d116af6cc10592caaecd8d1f38cd
    Author: drDaria <ddrobazkina@gmail.com>
    Date:   Wed Jan 15 15:14:44 2025 +0200
    
        Update page.tsx
    
    commit cbbb1822cd5b0705e0de20efa3a75676a542a8fa
    Author: drDaria <ddrobazkina@gmail.com>
    Date:   Wed Jan 15 15:13:31 2025 +0200
    
        Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,78 +28,80 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="min-h-screen bg-gradient-to-b from-gray-800 to-gray-900 text-white flex flex-col items-center p-8 space-y-10">
-      <h1 className="text-4xl font-extrabold text-yellow-400 tracking-wide">
-        🚗 Car Dealer App
-      </h1>
-      <p className="text-lg text-gray-300 text-center">
-        Select your dream car make and model year!
-      </p>
+    <div className="h-fit w-full lg:w-1/2 text-white flex flex-col items-center p-8 ">
+      <div className="h-fit  text-white flex flex-col items-center p-8 space-y-10 bg-gray-800 bg-opacity-50 rounded-xl">
+        <h1 className="text-4xl font-extrabold text-yellow-400 tracking-wide">
+          🚗 Car Dealer App
+        </h1>
+        <p className="text-lg  text-center">
+          Select your dream car make and model year!
+        </p>
 
-      <div className="bg-gray-700 p-6 rounded-lg shadow-lg w-full max-w-md space-y-6">
-        <div>
-          <label
-            htmlFor="makes"
-            className="block text-sm font-medium text-gray-300 mb-2"
-          >
-            Select Vehicle Make
-          </label>
-          {makes.length > 0 ? (
-            <select
-              id="makes"
-              className="w-full p-3 border border-gray-500 rounded-lg bg-gray-800 text-gray-100 focus:outline-none focus:ring-2 focus:ring-yellow-400"
-              value={selectedMake}
-              onChange={(e) => setSelectedMake(e.target.value)}
+        <div className="bg-gray-700 p-6 rounded-lg shadow-lg w-full max-w-md space-y-6">
+          <div>
+            <label
+              htmlFor="makes"
+              className="block text-sm font-medium text-gray-300 mb-2"
             >
-              <option value="">Select Vehicle Make</option>
-              {makes.map((make) => (
-                <option key={make.MakeId} value={make.MakeId}>
-                  {make.MakeName}
-                </option>
-              ))}
-            </select>
-          ) : (
-            <div className="flex items-center justify-center">
-              <Spinner />
-            </div>
-          )}
+              Select Vehicle Make
+            </label>
+            {makes.length > 0 ? (
+              <select
+                id="makes"
+                className="w-full p-3 border border-gray-500 rounded-lg bg-gray-800 text-gray-100 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                value={selectedMake}
+                onChange={(e) => setSelectedMake(e.target.value)}
+              >
+                <option value="">Select Vehicle Make</option>
+                {makes.map((make) => (
+                  <option key={make.MakeId} value={make.MakeId}>
+                    {make.MakeName}
+                  </option>
+                ))}
+              </select>
+            ) : (
+              <div className="flex items-center justify-center">
+                <Spinner />
+              </div>
+            )}
+          </div>
+
+          <div>
+            <label
+              htmlFor="years"
+              className="block text-sm font-medium text-gray-300 mb-2"
+            >
+              Select Model Year
+            </label>
+            {currentYear ? (
+              <select
+                id="years"
+                className="w-full p-3 border border-gray-500 rounded-lg bg-gray-800 text-gray-100 focus:outline-none focus:ring-2 focus:ring-yellow-400"
+                value={selectedYear}
+                onChange={(e) => setSelectedYear(e.target.value)}
+              >
+                <option value="">Select Model Year</option>
+                {Array.from({ length: currentYear - 2014 }, (_, i) => (
+                  <option key={i} value={2015 + i}>
+                    {2015 + i}
+                  </option>
+                ))}
+              </select>
+            ) : (
+              <div className="flex items-center justify-center">
+                <Spinner />
+              </div>
+            )}
+          </div>
         </div>
 
         <div>
-          <label
-            htmlFor="years"
-            className="block text-sm font-medium text-gray-300 mb-2"
-          >
-            Select Model Year
-          </label>
-          {currentYear ? (
-            <select
-              id="years"
-              className="w-full p-3 border border-gray-500 rounded-lg bg-gray-800 text-gray-100 focus:outline-none focus:ring-2 focus:ring-yellow-400"
-              value={selectedYear}
-              onChange={(e) => setSelectedYear(e.target.value)}
-            >
-              <option value="">Select Model Year</option>
-              {Array.from({ length: currentYear - 2014 }, (_, i) => (
-                <option key={i} value={2015 + i}>
-                  {2015 + i}
-                </option>
-              ))}
-            </select>
-          ) : (
-            <div className="flex items-center justify-center">
-              <Spinner />
-            </div>
-          )}
+          <NavigationButton
+            href={`/result/${selectedMake}/${selectedYear}`}
+            label="Next"
+            isDisabled={!selectedMake || !selectedYear}
+          />
         </div>
-      </div>
-
-      <div>
-        <NavigationButton
-          href={`/result/${selectedMake}/${selectedYear}`}
-          label="Next"
-          isDisabled={!selectedMake || !selectedYear}
-        />
       </div>
     </div>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
     container: {
       center: true,
       padding: "2rem",
+
     },
 
     extend: {},
